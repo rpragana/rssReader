9c02b1b07a5c143b52ce3907066e3cd50906a903
rpragana <rildo@pragana.net>
2013-06-07 07:10:54 -0300

    Removed forced gui flag.

--- rssReader.vfs/lib/tatu/tatu.tcl
+++ rssReader.vfs/lib/tatu/tatu.tcl
@@ -13,8 +13,7 @@
 package provide tatu 0.6
 
 ### use Tk send mechanism to debug via tkcon
-if { 1 || 
-	[string tolower [lindex $argv 0]] eq "gui" } {
+if {[string tolower [lindex $argv 0]] eq "gui" } {
 set ::GUI 1
 set argv [lrange $argv 1 end]
 package require Tk
